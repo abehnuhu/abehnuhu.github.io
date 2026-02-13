55a02600a7e5b13f491c19a3640f29f430b07ef1
abehnuhu <abehnuhu@gmail.com>
2026-02-13 04:14:03 +0000

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDl6pEFAsr6M_T-eAuE2FuMS3kOOp1LBKs",  // Note: 'l' not '1'
+    authDomain: "house-of-baiden.firebaseapp.com",
+    projectId: "house-of-baiden",
+    storageBucket: "house-of-baiden.firebasestorage.app",
+    messagingSenderId: "220065196721",
+    appId: "1:220065196721:web:8fe9ab0c991b8d04dc1090",
+    measurementId: "G-Z2RLV3V401"
+};
